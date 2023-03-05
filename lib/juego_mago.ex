476c68698f6ce02736f2e1eea1cf5816e3936001
AndersonElit <rodriarri@gmail.com>
2023-03-05 14:25:32 -0500

    creacion proyecto

---
+++ lib/juego_mago.ex
@@ -0,0 +1,18 @@
+defmodule JuegoMago do
+  @moduledoc """
+  Documentation for `JuegoMago`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JuegoMago.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/juego_mago_test.exs
@@ -0,0 +1,8 @@
+defmodule JuegoMagoTest do
+  use ExUnit.Case
+  doctest JuegoMago
+
+  test "greets the world" do
+    assert JuegoMago.hello() == :world
+  end
+end
